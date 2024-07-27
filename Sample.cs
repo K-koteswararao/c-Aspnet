27a8fa95336ea5ead61d0dd88a9680f426538939
Kantumajji koteswararao <101973018+K-koteswararao@users.noreply.github.com>
2024-07-27 10:24:59 +0530

    Add files via upload

---
+++ Sample.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First
+{
+    public class Sample
+    {
+        public int sum(int x, int y)
+        {
+            return x + y;
+        }
+        public int sub(int x, int y)
+        {
+            return x - y;
+        }
+    }
+}
